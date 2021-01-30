0c7f57f104ff66beab98e3badfe07a041c46be97
akio0911 <akio0911@gmail.com>
2021-01-30 22:06:32 +0900

    feedback

--- ChallengeTask7/ViewController.swift
+++ ChallengeTask7/ViewController.swift
@@ -9,13 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var label: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     @IBAction func plusButton(_ sender: Any) {
         let number1 = Int(textField1.text ?? "") ?? 0
         let number2 = Int(textField2.text ?? "") ?? 0
